62cfbd313ad1ecb77a87ed486f8a63a2e99b5e6f
Joshua <jawshanderson990@gmail.com>
2025-03-10 13:38:16 -0500

    Implemented standard swing UI

--- mao/src/main/java/com/mao/MainFrame.java
+++ mao/src/main/java/com/mao/MainFrame.java
@@ -2,5 +2,19 @@ package mao.src.main.java.com.mao;
 
 import javax.swing.JFrame;
 
+/*
+ * Notes:
+ * Everything should stem from this file
+ */
+
 public class MainFrame extends JFrame {
+    public MainFrame() {
+        super("Mao");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(950, 600);
+
+        Config.applySettings();
+
+        setVisible(true);
+    }
 }
