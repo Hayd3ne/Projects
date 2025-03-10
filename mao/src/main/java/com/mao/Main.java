0c2d7b759843640bc63ec7fbbcaf9314e550704f
Joshua <jawshanderson990@gmail.com>
2025-03-10 12:53:09 -0500

    Created MainFrame

--- mao/src/main/java/com/mao/Main.java
+++ mao/src/main/java/com/mao/Main.java
@@ -1,10 +1,15 @@
 package mao.src.main.java.com.mao;
 
+import javax.swing.SwingUtilities;
 
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+    public static void main( String[] args ) {
+    SwingUtilities.invokeLater(() -> { 
+            try {
+                new MainFrame(); 
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
---
+++ mao/src/main/java/com/mao/MainFrame.java
@@ -0,0 +1,6 @@
+package mao.src.main.java.com.mao;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+}
