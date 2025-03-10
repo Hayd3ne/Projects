565f09eb7868631f05c6df92f32304a3be552078
Joshua <jawshanderson990@gmail.com>
2025-03-10 13:38:01 -0500

    Updated Main.java

--- mao/src/main/java/com/mao/Main.java
+++ mao/src/main/java/com/mao/Main.java
@@ -2,6 +2,8 @@ package mao.src.main.java.com.mao;
 
 import javax.swing.SwingUtilities;
 
+// Dont code anything here, all this is meant to do is start the MainFrame and everything branches from there.
+
 public class Main {
     public static void main( String[] args ) {
     SwingUtilities.invokeLater(() -> { 
